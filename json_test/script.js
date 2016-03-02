3134cecb67fa367e1cacfe02a080210963969462
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2016-03-02 17:56:41 +0900

    Added ajax_test/, json_test/

--- json_test/script.js
+++ json_test/script.js
@@ -1,30 +1,44 @@
 var DEBUG = true;
 
-var messageList = [ "やふー", "こなものだいすきー", "きょうじ、きょうじー", "みのり！"];
 var messageNum = 0;
 
 document.addEventListener( 'DOMContentLoaded', function( eve) {
+	$.ajax({
+	  url: "/charactor.json",
+	  data: {
+	    zipcode: 97201
+	  },
+	  success: function( data ) {
+	    $( "#weather-temp" ).html( "<strong>" + data + "</strong> degrees" );
+	  }
+	});
+
+	// charactor data
+	var charctorData = '{"name" : "pierre", "message" : [ "やふー", "こなものだいすきー", "きょうじ、きょうじー", "みのり！"]}';
+	var messageL = JSON.parse( charctorData);
+
+	// Message Box
 	var messageBox = document.getElementById( 'messageBox');
+	// Print argument
 	messageBox.print = function( message) {
 		this.innerHTML = "<p>" + message + "</p>";
 	};
 
 	var message = document.getElementById( "message");
 
-	messageBox.print( messageList[ messageNum]);
+	messageBox.print( messageL[ "message"][ messageNum]);
 
 	messageBox.addEventListener( 'click', function( eve) {
 		messageNum++;
-		if( messageNum > messageList.length-1) messageNum = 0;
-		this.print( messageList[ messageNum]);
+		if( messageNum > messageL[ "message"].length-1) messageNum = 0;
+		this.print( messageL[ "message"][ messageNum]);
 
 	}, false);
 
 }, false);
 
-/* Debug comment
-	debugLog( "unko"); -> unko
-*/
+// Debug comment
+//	debugLog( "unko"); -> unko
 function debugLog( obj) {
 	if ( DEBUG) console.log( obj);
 }
