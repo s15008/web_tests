c9cb1d7a8127320368c4c18e6448624519ce6aa5
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2016-03-11 14:30:17 +0900

    Add Player, Enemy, LifeBar Objects

--- clickapp_test/main.js
+++ clickapp_test/main.js
@@ -1,24 +1,32 @@
 var ATTACK_POINT_DEF = 100;
 var ENEMY_LIFE_DEF = 400;
+var ENEMY_IMG_URL = "img/milkSake.png";
 
 $( function() {
 	var Player = function() {
 		this.attackPoint = ATTACK_POINT_DEF;
 	};
 
-	var Enemy = function( player){
+	var Enemy = function( player, imageUrl){
+		$( "#enemy").append( '<img id="enemyImg" src="' + imageUrl + '">');
 		this.player = player;
 		this.maxLife = ENEMY_LIFE_DEF;
+		this.image = $( "#enemyImg");
 		this.life = this.maxLife;
+		this.dead = false;
 	};
 
 	Enemy.prototype.hit = function() {
 		this.life -= this.player.attackPoint;
 		console.log( "enlife : ", this.life);
+
+		if ( this.life <= 0) {
+			this.dead = true;
+		}
 	};
 
 	var LifeBar = function( charactor) {
-		this.bar = $( "#EnemyLifeBar");
+		this.bar = $( "#enemyLifeBar");
 		this.maxWidth = this.bar.width();
 		this.charactor = charactor;
 	};
@@ -27,20 +35,30 @@ $( function() {
 		var percent = 100 / ( this.charactor.maxLife / this.charactor.life);
 		var currentWidth = percent / 100 * this.maxWidth;
 		var shiftWidth = ( this.maxWidth - currentWidth) / 2;
-		console.log( currentWidth, shiftWidth);
 
-		this.bar.width( currentWidth);
-		this.bar.css( 'left', -shiftWidth);
+		// View
+		this.bar.animate( {
+			width: currentWidth,
+			left: -shiftWidth
+		}, 300);
+		//this.bar.width( currentWidth);
+		//this.bar.css( 'left', -shiftWidth);
 	};
 
+	// Image preloading
+	var enemyImage = "images/milkShake.png";
+	$("<img>").attr("src", enemyImage);
+
 	var player = new Player();
-	var enemy = new Enemy( player);
+	var enemy = new Enemy( player, enemyImage);
 	var lifeBar = new LifeBar( enemy);
 
 	$( window).on( 'click', function( eve) {
-		console.log( "hit");
 		enemy.hit();
 		lifeBar.update();
+		if ( enemy.dead) {
+			enemy.image.fadeOut( 2000);
+		}
 	});
 
 });
